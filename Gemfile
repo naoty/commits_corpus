7edf9b63bcaabee342951497cfbbaefce2ee83a8
Naoto Kaneko <naoty.k@gmail.com>
2013-12-21 21:51:12 +0900

    Search commit messages with elasticsearch client

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem "jquery-rails"
 gem "turbolinks"
 gem "jbuilder", "~> 1.2"
 gem "bootstrap-sass-rails", "~> 3.0.3.0"
+gem "elasticsearch", "~> 0.4.3"
 
 group :doc do
   gem "sdoc", require: false
--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -3,5 +3,17 @@ class CommitsController < ApplicationController
   end
 
   def index
+    body = {}
+    body[:query] = {}
+    body[:query][:match] = {}
+    body[:query][:match][:message] = params[:keyword]
+    results = client.search(index: "commits", body: body)
+    @commits = results["hits"]["hits"].map { |result| result["_source"] }
+  end
+
+  private
+
+  def client
+    Elasticsearch::Client.new
   end
 end
